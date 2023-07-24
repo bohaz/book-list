b13dd71d653ab7afd066cd5398d8e357b1b62c59
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-07-24 18:10:32 -0400

    Add reducers and actions

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+  status: 'Under construction',
+};
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+  },
+});
+
+export default categoriesSlice.reducer;
